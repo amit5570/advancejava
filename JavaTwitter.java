e3c307e7bfc4efc413cc38e2bda8def2f8592c1b
deep_malde <maldedeep8@gmail.com>
2020-02-15 07:48:49 +0530

    advancejava

---
+++ JavaTwitter.java
@@ -0,0 +1,28 @@
+package sycs69;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.*;
+public class JavaTwitter {
+
+
+public static void main(String[] args) {
+
+try{
+URL url = new URL("https://jsonplaceholder.typicode.com/todos/1");
+URLConnection urlCon = (HttpURLConnection) url.openConnection();
+
+
+BufferedReader br = new BufferedReader(new InputStreamReader(urlCon.getInputStream()));
+String temp = null;
+while ((temp = br.readLine()) != null)
+{
+
+System.out.println(temp);
+}
+
+}
+catch(Exception e) {e.printStackTrace();}
+}
+
+}
